311008073ecae2047ea80314fc2f8466a6e02d14
ttagawa <tylertagawa@gmail.com>
2017-02-28 17:59:55 -0800

    Added webapp to heroku.

--- app.js
+++ app.js
@@ -13,11 +13,20 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Send the index.html file 
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.use("/static", express.static(__dirname + '/static'));
 // Require our routes into the application.
 require('./server/routes')(app);
 app.get('*', (req, res) => res.status(200).send({
   message: 'Welcome to the beginning of nothingness.',
 }));
+
+// send static files for webapp
+app.use("/static", express.static(__dirname + '/static'));
 
 app.listen(port);
 console.log("Listening to port: " + port);
